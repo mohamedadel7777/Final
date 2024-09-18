0473ff2ae24b941fb5158cae03e6e2ca361a9c9c
mohamedadel7777 <elmorsym04@gmail.com>
2024-09-19 02:53:19 +0300

    added

---
+++ server.js
@@ -0,0 +1,11 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // db.json should contain your JSON data
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
